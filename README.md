5ea27dde4077154a47d246df8a71651b815277d8
ptabis <p.tabis@protonmail.com>
2020-02-25 18:00:23 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+__Compile with `npx webpack` command__
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    mode: "production",
+
+    devtool: "source-map",
+    entry: "./src/index.ts",
+    resolve: {
+        extensions: [".ts"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    }
+};
